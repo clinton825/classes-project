2e0a31eb8c6f7ca34b9acb02ee6d47d2072ff7cf
clintonbempah <20097793@mail.wit.ie>
2023-02-02 18:23:16 +0000

    in this section i added Lambdas, using Lambdas to iterate over collections Using Lambdas to Filter Collections Using Lambdas to Sort Collections Using Lambdas to display in Uppercase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,49 @@
-fun main(args: Array<String>) {
-    val person1 = Person("Mark","Roche")
+
+private val persons = ArrayList<Person>()
+fun main() {
+    persons.add(Person("Mark","Roche"))
+    persons.add(Person("John","Walsh"))
+    persons.add(Person("Sheila","Flynn"))
+
+    for ((index, value )in persons.withIndex())
+        println("$index: $value")
+
+    println("\nFiltering first name as John:")
+    persons
+        .filter { it.firstName.contains("John") }
+    .forEach{it -> println(it) }
+
+    println("\nSorting by last name:")
+    persons
+        .sortedBy { it.lastName }
+        .forEach{ println(it) }
+
+    println("\nSorting by last name, last name uppercased")
+    persons
+        .sortedBy { it.lastName }
+        .forEach{ println("Person: ${it.firstName}, ${it.lastName.uppercase()}") }
+}
+
+
+fun personInfo() {
+    val person1 = Person("Mark", "Roche")
     val person2 = person1.copy(firstName = " Clare")
     val person3 = person1.copy(firstName = "James")
     println(person1)
     println(person2.toString())
     println(person3)
+// copy(), equals(), hashcode()
+    println("person1 hashcode = ${person1.hashCode()}")
+    println("person2 hashcode = ${person2.hashCode()}")
+    println("person3 hashcode = ${person3.hashCode()}")
 
+    if (person1.equals(person2))
+        println("person1 is equal to person2.")
+    else
+        println("person1 is not equal to person2.")
 
+    if (person1.equals(person3))
+        println("person1 is equal to person3.")
+    else
+        println("person1 is not equal to person3 ")
 }
